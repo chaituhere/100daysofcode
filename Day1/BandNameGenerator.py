54346755f7881469519dc722824794af17a0113a
chaituhere <chaysinfo@gmail.com>
2025-01-05 17:29:00 +0530

    Create BandNameGenerator.py

---
+++ Day1/BandNameGenerator.py
@@ -0,0 +1,6 @@
+# Day 1: Printing, Commenting, Debugging, String Manipulation & Variables
+# Band Name Generator Project
+print("Welcome to the Band Name Generator!")
+cityname = input("What's the city you grew up in?\n")
+petname = input("What's your pet's name?\n")
+print("Your band name could be: " + cityname + " " + petname)

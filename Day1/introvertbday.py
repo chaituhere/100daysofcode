611552766822962e1b8f3de13fd146d97452b96b
chaituhere <chaysinfo@gmail.com>
2025-01-05 17:44:41 +0530

    Create introvertbday.py

---
+++ Day1/introvertbday.py
@@ -0,0 +1,6 @@
+# Birthday Greeter for Introverts
+print("Welcome to the Birthday Greeter!")
+name = input("Can I take your name, please, love?\n")
+print("Happy Birthday, " + name + "!")
+print("(*_*)")
+print("/>🎂 here's your bday cake.")
